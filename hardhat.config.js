d07980ff86349d76cec63f8cc02c7fe34db7da5b
Tolga Erdonmez <tolgaerdonmez@gmail.com>
2023-02-20 19:27:58 +0100

    deploying added

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,8 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: { url: "http://127.0.0.1:8545/", chainId: 31337, },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,15 @@ require("dotenv").config;
 async function main() {
   const ToDoStorageFactory = await ethers.getContractFactory("ToDoStorage");
   console.log("Deploying...");
-  const todoStorage = await ToDoStorageFactory.deploy({
-    log: true,
-  });
+  const todoStorage = await ToDoStorageFactory.deploy();
   await todoStorage.deployed();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch(error => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
