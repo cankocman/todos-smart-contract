e5c1fcfbccee7d06081498e9b3f4ed78c4d6d12c
Can Koçman <cankocman@Cans-MacBook-Air-4.local>
2023-02-20 12:12:56 +0300

    Contract completed𝌕
    The way contract stores to-do lists has been updated.
    Status of to-do items has been added.
    Removing to-do lists, to-do list items and updating to-do list items functions have been added.

--- contracts/ToDoStorage.sol
+++ contracts/ToDoStorage.sol
@@ -5,28 +5,67 @@ contract ToDoStorage {
     struct ToDoList {
         uint256 id;
         string name;
-        string[] todos;
+        string[2][] todos;
     }
 
-    mapping(address => mapping(uint256 => ToDoList)) public addressToList;
+    mapping(address => ToDoList[]) public addressToList;
     uint256 private _nonce = 0;
 
-    function createToDoList(string memory name, string[] memory todos) public {
+    function createToDoList(
+        string memory name,
+        string[] memory contents
+    ) public {
         uint256 id = createId();
-        addressToList[msg.sender][id] = ToDoList(id, name, todos);
+        string[2][] memory todos = new string[2][](contents.length);
+        for (uint256 i = 0; i < contents.length; i++) {
+            todos[i] = [contents[i], "false"];
+        }
+        addressToList[msg.sender].push(ToDoList(id, name, todos));
     }
 
     function createTodo(uint256 todoListId, string memory content) public {
-        addressToList[msg.sender][todoListId].todos.push(content);
+        (bool found, uint256 todoListIndex) = findToDoListIndex(todoListId);
+        if (!found) return;
+        addressToList[msg.sender][todoListIndex].todos.push([content, "false"]);
     }
 
-    function retrieve(uint256 id) public view returns (string[] memory) {
-        return addressToList[msg.sender][id].todos;
+    function updateTodo(
+        uint todoListId,
+        uint256 todoItemId,
+        string[2] memory todo
+    ) public {
+        (bool found, uint256 todoListIndex) = findToDoListIndex(todoListId);
+        if (!found) return;
+        addressToList[msg.sender][todoListIndex].todos[todoItemId] = todo;
     }
 
-    // function getToDoLists() public view returns (mapping(uint256 => ToDoList) meory) {
-    //     return addressToList[msg.sender];
-    // }
+    function retrieve(uint256 id) public view returns (string[2][] memory) {
+        (bool found, uint256 todoListIndex) = findToDoListIndex(id);
+        if (!found) return new string[2][](0);
+
+        return addressToList[msg.sender][todoListIndex].todos;
+    }
+
+    function removeTodo(uint todoListId, uint256 todoItemId) public {
+        (bool found, uint256 todoListIndex) = findToDoListIndex(todoListId);
+        if (!found) return;
+        delete addressToList[msg.sender][todoListIndex].todos[todoItemId];
+    }
+
+    function removeTodoList(uint todoListId) public {
+        (bool found, uint256 todoListIndex) = findToDoListIndex(todoListId);
+        if (!found) return;
+        delete addressToList[msg.sender][todoListIndex];
+    }
+
+    function findToDoListIndex(uint256 id) public view returns (bool, uint256) {
+        for (uint256 i = 0; i < addressToList[msg.sender].length; i++) {
+            if (addressToList[msg.sender][i].id == id) {
+                return (true, i);
+            }
+        }
+        return (false, 0);
+    }
 
     function createId() private returns (uint256) {
         _nonce++;
